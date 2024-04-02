a9a0ec2044bc7c27d690ef1f0e93c86fe5eb64b7
Null <kelanidarasimi9@gmail.com>
2024-04-02 12:20:56 +0100

    get a stats of all objects

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Blueprint for the flask application endpoints"""
+from os import getenv
 from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-
+storage_type = getenv('HBNB_TYPE_STORAGE')
 from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,30 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.amenity import Amenity
+from models.user import User
+from models import storage
 
-
+OBJECTS = [
+    Amenity, Place, Review,
+    City, State, User
+]
 @app_views.route('/status', methods=['GET'])
 def status():
     """return an ok status"""
     return jsonify({"status": "OK"})
+
+    
+    
+@app_views.route('/stats', methods=['GET'])
+def object_stats():
+    """get a stat on all objects"""
+    obj_stats = {}
+    for val in OBJECTS:
+        obj_stats[val.__tablename__] = storage.count(val)
+    
+    return jsonify(obj_stats)
