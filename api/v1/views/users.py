d9ebb23d85e352c77505f78677d8cc813c4da246
Null <kelanidarasimi9@gmail.com>
2024-04-02 15:29:49 +0100

    crud operations on users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""CRUD operation on users object"""
+"""Perform Crud operations on users"""
 
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
